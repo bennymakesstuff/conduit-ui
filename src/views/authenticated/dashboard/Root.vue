917fa25c006507ff5a3a4183772513c370e62401
Benny Makes <me@benbroad.com>
2022-01-13 03:58:11 +1030

    Adds grids to Dahsboard

--- src/views/authenticated/dashboard/Root.vue
+++ src/views/authenticated/dashboard/Root.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>
+  <div class="inner-content">
 
     <div class="default_title">
       <h1>{{ 'Hi, ' + user.firstname }}</h1>
+    </div>
+
+    <div class="dash-grid">
+      <div class="p-grid">
+        <div class="p-col-4 p-m-2">4</div>
+        <div class="p-col p-m-2">1</div>
+        <div class="p-col p-m-2">1</div>
+        <div class="p-col p-m-2">1</div>
+        <div class="p-col p-m-2">1</div>
+        <div class="p-col p-m-2">1</div>
+        <div class="p-col p-m-2">1</div>
+        <div class="p-col p-m-2">1</div>
+        <div class="p-col p-m-2">1</div>
+      </div>
+
+      <div class="p-grid">
+        <div class="p-col-2 p-m-2 p-shadow-1">2</div>
+        <div class="p-col-6 p-m-2">6</div>
+        <div class="p-col-4 p-m-2">4</div>
+      </div>
     </div>
 
   </div>
@@ -32,5 +52,16 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/assets/theme/main.scss';
+
+.inner-content {
+  padding: 1rem;
+}
+
+.dash-grid > div > div {
+  background-color: #ffffff;
+  //border: 1px solid #d9d9d9;
+  border-radius: 0.25rem;
+  box-shadow: 1px 0px 2px 1px rgba(187, 187, 187, 0.23);
+}
 
 </style>
