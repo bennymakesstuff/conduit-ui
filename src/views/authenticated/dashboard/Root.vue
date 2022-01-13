74dbce5f8cb114f53c433bac0c374642aa93127b
Benny Makes <me@benbroad.com>
2022-01-14 00:36:57 +1030

    Removes unused dashboard empty widgets

--- src/views/authenticated/dashboard/Root.vue
+++ src/views/authenticated/dashboard/Root.vue
@@ -9,19 +9,6 @@
       <div class="p-grid">
         <ActiveUsers />
         <ActiveRoles />
-        <div class="p-col p-m-2">1</div>
-        <div class="p-col p-m-2">1</div>
-        <div class="p-col p-m-2">1</div>
-        <div class="p-col p-m-2">1</div>
-        <div class="p-col p-m-2">1</div>
-        <div class="p-col p-m-2">1</div>
-        <div class="p-col p-m-2">1</div>
-      </div>
-
-      <div class="p-grid">
-        <div class="p-col-2 p-m-2 p-shadow-1">2</div>
-        <div class="p-col-6 p-m-2">6</div>
-        <div class="p-col-4 p-m-2">4</div>
       </div>
     </div>
 
